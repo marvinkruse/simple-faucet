bb1b736ca3b3535a62db055a49535b068d713385
rykci <yuen.ricky@hotmail.com>
2021-12-08 11:07:42 -0500

    changed waitTime to 24 hrs and modified requestToken to transfer as the last step

--- smartcontract/SwanFaucet.sol
+++ smartcontract/SwanFaucet.sol
@@ -1,33 +1,36 @@
-pragma solidity ^0.5.1;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 interface ERC20 {
     function transfer(address to, uint256 value) external returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-contract Faucet {
-    uint256 constant public tokenAmount = 100000000000000000000;
-    uint256 constant public waitTime = 30 minutes;
+contract SwanFaucet {
+    uint256 constant public tokenAmount = 100000000000000000000; //100 token
+    uint256 constant public waitTime = 24 hours;
 
     ERC20 public tokenInstance;
     
-    mapping(address => uint256) lastAccessTime;
+    mapping(address => uint256) nextAccessTime;
 
-    constructor(address _tokenInstance) public {
+    constructor(address _tokenInstance)  {
         require(_tokenInstance != address(0));
         tokenInstance = ERC20(_tokenInstance);
     }
 
     function requestTokens() public {
         require(allowedToWithdraw(msg.sender));
+        nextAccessTime[msg.sender] = block.timestamp + waitTime;
+
+        // transfer after setting internal state
         tokenInstance.transfer(msg.sender, tokenAmount);
-        lastAccessTime[msg.sender] = block.timestamp + waitTime;
     }
 
     function allowedToWithdraw(address _address) public view returns (bool) {
-        if(lastAccessTime[_address] == 0) {
+        if(nextAccessTime[_address] == 0) {
             return true;
-        } else if(block.timestamp >= lastAccessTime[_address]) {
+        } else if(block.timestamp >= nextAccessTime[_address]) {
             return true;
         }
         return false;

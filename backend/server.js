fafb127714782605192971d95ca6be27abab58bc
rykci <yuen.ricky@hotmail.com>
2021-12-21 13:14:34 -0500

    backend check faucet balance and return status

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,9 @@ const privateKey = process.env.PRIVATE_KEY
 
 web3.eth.accounts.wallet.add(privateKey) // adds account using private key
 
-//const faucetAddress = '0x099e67a3f29B16C6FFCC621f3c7Ddf64eAfBf632'
+//const faucetAddress = '0x099e67a3f29B16C6FFCC621f3c7Ddf64eAfBf632' // old faucet
 const faucetAddress = process.env.FAUCET_ADDRESS
+//const faucetAddress = '0x03ab3412db32c99c21f3499c3fca497948756c70' //test faucet
 const faucetInterface = [
   {
     inputs: [
@@ -42,6 +43,15 @@ const faucetInterface = [
     type: 'function',
   },
 ]
+const tokenInterface = [
+  {
+    constant: true,
+    inputs: [{ name: '_owner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: 'balance', type: 'uint256' }],
+    type: 'function',
+  },
+]
 const faucetContract = new web3.eth.Contract(faucetInterface, faucetAddress, {
   from: walletAddress,
 })
@@ -60,6 +70,9 @@ app.post('/', async (req, res) => {
     ),
   )
 
+  // get faucet balance status, also remove addressese from array
+  const faucetStatus = await checkFaucetBalance(tokenAddresses, tokenAmounts)
+
   try {
     const transaction = await faucetContract.methods
       .sendMultiTokens(tokenAddresses, tokenAmounts, checkSumAddress)
@@ -67,7 +80,7 @@ app.post('/', async (req, res) => {
 
     console.log(transaction.transactionHash)
 
-    res.json({ message: 'success', tx_hash: transaction.transactionHash })
+    res.json([...faucetStatus, { tx_hash: transaction.transactionHash }])
   } catch (err) {
     console.log(err)
     res.status(500).json(err)
@@ -77,3 +90,42 @@ app.post('/', async (req, res) => {
 app.listen(port, () => {
   console.log(`Faucet server listening at http://localhost:${port}`)
 })
+
+// return array of status objects for each token address and filter arrays for available tokens only
+const checkFaucetBalance = async (tokenAddresses, tokenAmounts) => {
+  let status = []
+
+  for (let i = 0; i < tokenAddresses.length; i++) {
+    let faucetBalance = 0
+    let addressStatus = { address: tokenAddresses[i], result: 0 }
+
+    // get faucet balance
+    if (tokenAddresses[i] == '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE') {
+      faucetBalance = await web3.eth.getBalance(faucetAddress) // get MATIC balance
+    } else {
+      let tokenContract = new web3.eth.Contract(
+        tokenInterface,
+        tokenAddresses[i],
+      )
+      faucetBalance = await tokenContract.methods
+        .balanceOf(faucetAddress)
+        .call()
+    }
+
+    // if there is not enough tokens in faucet
+    if (parseFloat(faucetBalance) < parseFloat(tokenAmounts[i])) {
+      // change the token status
+      addressStatus.result = -1
+      addressStatus.err = 'running out of tokens'
+
+      // remove from array
+      tokenAddresses.splice(i, 1)
+      tokenAmounts.splice(i, 1)
+      i--
+    }
+
+    status.push(addressStatus)
+  }
+
+  return status
+}
